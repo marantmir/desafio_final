a4a3a229953e6fcd597d663cc8d085d5f8cc75db
Silvio <silviolima07@gmail.com>
2026-03-24 14:08:54 -0300

    Update app Streamlit

--- app.py
+++ app.py
@@ -48,7 +48,12 @@ def load_model(model_path):
     return joblib.load(model_path)
 
 try:
-    model = load_model('modelo/lgbm_best_full_model_pipeline.pkl')
+    model = load_model('modelo/lasso_best_full_model_pipeline.pkl')
+    model_ridge = load_model('modelo/Ridge_best_full_model_pipeline_3.pkl')
+    model_gb    = load_model('modelo/GradientBoosting_best_full_model_pipeline_3.pkl')
+    model_xgb   = load_model('modelo/XGBoost_best_full_model_pipeline_3.pkl')
+    model_rf    = load_model('modelo/RandomForest_best_full_model_pipeline_3.pkl')
+    
 except Exception as e:
     st.error(f"Erro ao carregar o modelo: {e}")
     st.stop()
@@ -63,7 +68,7 @@ df_carros = load_data()
 def exibir_recomendacoes(valor_estimado):
     st.divider()
     st.markdown('### Recomendação:')
-    st.subheader("Veja opções reais na mesma faixa de preço")
+    st.subheader("Veja opções na mesma faixa de preço (10%) ")
     
     limite_inferior = valor_estimado * 0.90
     limite_superior = valor_estimado * 1.10
@@ -95,25 +100,39 @@ def exibir_recomendacoes(valor_estimado):
 with st.form("form_previsao"):
     st.subheader("Dados do Veículo")
     
-    col1, col2 = st.columns(2)
+    col1, col2, col3 = st.columns(3)
     
     with col1:
         marca = st.selectbox("Marca", options=['Nissan', 'Ford', 'Toyota', 'Renault', 'Fiat', 'Jeep', 'Honda', 'Volkswagen', 'Hyundai', 'Chevrolet'])
-        modelo = st.selectbox("Modelo", options=['Frontier', 'Ranger', 'Hilux', 'Sandero', 'Duster', 'Kicks', 'Ka', 'Corolla', 'Mobi', 'Renegade', 'Compass', 'HR-V', 'T-Cross', 'Toro', 'HB20S', 'Yaris', 'EcoSport', 'Onix', 'Polo', 'Argo', 'Kwid', 'Virtus', 'Civic', 'Cronos', 'Gol', 'Versa', 'Creta', 'HB20', 'S10', 'Tracker', 'Onix Plus', 'Fit'])
-        ano = st.number_input("Ano de Fabricação", min_value=1950, max_value=2026, value=2018, step=1)
-        quilometragem = st.number_input("Quilometragem (km)", min_value=0, max_value=1000000, value=50000, step=1000)
+        #modelo = st.selectbox("Modelo", options=['Frontier', 'Ranger', 'Hilux', 'Sandero', 'Duster', 'Kicks', 'Ka', 'Corolla', 'Mobi', 'Renegade', 'Compass', 'HR-V', 'T-Cross', 'Toro', 'HB20S', 'Yaris', 'EcoSport', 'Onix', 'Polo', 'Argo', 'Kwid', 'Virtus', 'Civic', 'Cronos', 'Gol', 'Versa', 'Creta', 'HB20', 'S10', 'Tracker', 'Onix Plus', 'Fit'])
+        #ano = st.number_input("Ano de Fabricação", min_value=1950, max_value=2026, value=2018, step=1)
+        #quilometragem = st.number_input("Quilometragem (km)", min_value=0, max_value=1000000, value=50000, step=1000)
     
     with col2:
-        cor = st.selectbox("Cor", options=['Cinza', 'Preto', 'Branco', 'Azul', 'Prata', 'Vermelho'])
-        cambio = st.selectbox("Câmbio", options=['Manual', 'Automático'])
-        combustivel = st.selectbox("Combustível", options=['Gasolina', 'Flex', 'Diesel'])
-        portas = st.number_input("Número de Portas", min_value=2, max_value=5, value=4, step=1)
+        quilometragem = st.number_input("Quilometragem (km)", min_value=0, max_value=1000000, value=50000, step=1000)
+        #cor = st.selectbox("Cor", options=['Cinza', 'Preto', 'Branco', 'Azul', 'Prata', 'Vermelho'])
+        #cambio = st.selectbox("Câmbio", options=['Manual', 'Automático'])
+        #combustivel = st.selectbox("Combustível", options=['Gasolina', 'Flex', 'Diesel'])
+        #portas = st.number_input("Número de Portas", min_value=2, max_value=5, value=4, step=1)
+        
+    with col3:
+        ano = st.number_input("Ano de Fabricação", min_value=1950, max_value=2026, value=2018, step=1)
+        #cor = st.selectbox("Cor", options=['Cinza', 'Preto', 'Branco', 'Azul', 'Prata', 'Vermelho'])
+        #cambio = st.selectbox("Câmbio", options=['Manual', 'Automático'])
+        #combustivel = st.selectbox("Combustível", options=['Gasolina', 'Flex', 'Diesel'])
+        #portas = st.number_input("Número de Portas", min_value=2, max_value=5, value=4, step=1)    
 
     submit_button = st.form_submit_button(label="🔍 Prever Valor de Venda")
-    st.error("Dados ficticios")
+    
+    html_page_aviso = """
+     <div style="background-color:black;padding=60px">
+         <p style='text-align:center;font-size:20px;font-weight:bold'>Dados Ficticios</p>
+     </div>
+               """               
+st.markdown(html_page_aviso, unsafe_allow_html=True)
 
 if submit_button:
-    if marca and modelo and cor:
+    if marca and quilometragem and ano:
         # 1. Tratar os dados
         # A nova feature foi criada a partir de 2026 menos o Ano
         idade_carro = 2026 - ano
@@ -121,14 +140,14 @@ if submit_button:
         # 2. Criar DataFrame com as entradas
         dados_entrada = {
             'Marca': [marca],
-            'Modelo': [modelo],
+            #'Modelo': [modelo],
             'Ano': [ano],
             'Quilometragem': [quilometragem],
-            'Cor': [cor],
-            'Cambio': [cambio],
-            'Combustivel': [combustivel],
-            'Portas': [portas],
-            'idade_carro': [idade_carro]
+            #'Cor': [cor],
+            #'Cambio': [cambio],
+            #'Combustivel': [combustivel],
+            #'Portas': [portas],
+            #'idade_carro': [idade_carro]
         }
         
         df_entrada = pd.DataFrame(dados_entrada)
@@ -136,11 +155,34 @@ if submit_button:
         # 3. Fazer a previsão
         try:
             # Tenta prever assumindo que o modelo contém o pipeline com o OneHotEncoder
-            previsao = model.predict(df_entrada)
-            valor_estimado = previsao[0]
+            valor_estimado = model.predict(df_entrada)[0]
+            valor_estimado_ridge = model_ridge.predict(df_entrada)[0]
+            valor_estimado_gb = model_gb.predict(df_entrada)[0]
+            valor_estimado_xgb = model_xgb.predict(df_entrada)[0]
+            valor_estimado_rf = model_rf.predict(df_entrada)[0]
+           
             
             st.success("✅ Previsão realizada com sucesso!")
-            st.metric(label="Valor de Venda Estimado", value=f"R$ {valor_estimado:,.2f}".replace(",", "X").replace(".", ",").replace("X", "."))
+            #st.metric(label="Valor de Venda Estimado", value=f"R$ {valor_estimado:,.2f}".replace(",", "X").replace(".", ",").replace("X", "."))
+            
+            lasso = f"{valor_estimado:,.2f}".replace(",", "X").replace(".", ",").replace("X", ".")
+            ridge = f"{valor_estimado_ridge:,.2f}".replace(",", "X").replace(".", ",").replace("X", ".")
+            xgb = f"{valor_estimado_xgb:,.2f}".replace(",", "X").replace(".", ",").replace("X", ".")
+            gb = f"{valor_estimado_gb:,.2f}".replace(",", "X").replace(".", ",").replace("X", ".")
+            rf = f"{valor_estimado_rf:,.2f}".replace(",", "X").replace(".", ",").replace("X", ".")
+           
+           
+            data = {
+                    'Modelo': ['Lasso', 'Ridge', 'XGB', 'GB', 'RF'],
+                    'Valor Estimado (R$)': [lasso, ridge, xgb, gb, rf ]
+                     }
+
+            df = pd.DataFrame(data)
+            df_melt = df.set_index('Modelo').T
+            st.dataframe(df_melt)
+
+            
+            
             exibir_recomendacoes(valor_estimado)
             
         except Exception as e:
